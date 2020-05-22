89be15c05f77ae985d9e69c29752f510c0a72486
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-05-22 20:04:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcule o índice de massa corporal de uma pessoa (IMC), peso de 80kg e altura de 2m.");
+            int P=80;
+            int A=2;
+            double resultado=Math.Pow(A, 2);
+            double resultado2=P / resultado;
+            Console.ReadKey();
+            Console.WriteLine($"IMC: {resultado2} kg/m²");
+        }
+    }
+}
